f1a833abcab15e09a9fa1edfb91a06c6d6c61b60
Fabiana Assami <bianavic@gmail.com>
2020-05-04 10:25:31 -0300

    cuidados com variavel global, init, property

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,21 @@
+import modelo.Cliente
+import modelo.ContaPoupanca
+import modelo.totalContas
 import teste.testaAutenticacao // importacao de funcao
 import teste.testaContasDiferentes
 
+// variavel global == evitar porque qquer um pode alterar
+//var totalContas = 0
+
 fun main() {
+
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+
     // testaAutenticacao()
     testaContasDiferentes()
+
+    //println("total de contas ${contaPoupanca.total}")
+    println("total de contas ${totalContas}")
+
 }
--- src/modelo/Conta.kt
+++ src/modelo/Conta.kt
@@ -1,11 +1,23 @@
 package modelo
 
+// property global
+var totalContas = 0
+private set
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+    var total = 0 // cada membro que criamos é exclusivo da instancia/objeto,
+    // por isso nao conseguimos armazenar valores entre as classes
+
+    // similar comportamento construtor secundario
+    init {
+        println("criando conta")
+        totalContas++ // variavel global, que todo mundo pode alterar
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
