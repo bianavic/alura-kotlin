96c6201978680e7889fb0ec6a580436344945deb
Fabiana Assami <bianavic@gmail.com>
2020-05-05 16:45:01 -0300

    executando programa com depurador

--- src/main.kt
+++ src/main.kt
@@ -1,41 +1,23 @@
-import modelo.Endereco
-import javax.print.attribute.IntegerSyntax
+// PILHA DE EXECUCAO | CALL STACK
 
-// no kotlin, qdo trabalharmos com TIPOS PRIMITIVOS, eles sempre serao CLASSES
-// tudo que trabalhar no kotlin é um objeto
+// DEPURADOR | DEBUGGER
 
 fun main() {
-    // sobrecarga implicita
-    val endereco = Endereco(
-        logradouro = "Rua Vergueiro",
-        complemento = "Alura",
-        cep = "00000"
-    )
-    val enderecoNovo = Endereco(
-        bairro = "Vila Mariana",
-        numero = 100,
-        cep = "00000"
-    )
-
-    // EQUALS == para ele o que importa é a referencia do objeto e nao a informacao que contem
-    println(endereco.equals(enderecoNovo)) // FALSE
-    println(endereco.equals(endereco)) // TRUE
-
-    // HASHCODE IDENTIFICACAO do objeto
-    println(endereco.hashCode())
-    println(enderecoNovo.hashCode())
-
-    // TO STRING = retorna REPRESENTACAO do objeto
-    println(endereco.toString())
-    println(enderecoNovo.toString())
-
-    // outra forma de imprimir em String observe que é na linguem java.
-    println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
-
-
+    println("Inicio main")
+    funcao1()
+    println("Fim main")
 }
 
-// sobrecarga = diferentes implementacoes para uma mesma assinatura
-fun imprime(valor: Any) {
-    println(valor)
+fun funcao1() {
+    println("Inicio funcao1")
+    funcao2()
+    println("Fim funcao1")
+}
+
+fun funcao2() {
+    println("Inicio funcao2")
+    for (i in 1..5) {
+        println(i)
+    }
+    println("Fim funcao2")
 }
