713e4bdb8adba4b35505af6f23533fb4d7d70304
Fabiana Assami <bianavic@gmail.com>
2020-05-05 13:30:18 -0300

    smart casting

--- src/main.kt
+++ src/main.kt
@@ -1,30 +1,21 @@
 import modelo.Endereco
 
+// no kotlin, qdo trabalharmos com TIPOS PRIMITIVOS, eles sempre serao CLASSES
+// tudo que trabalhar no kotlin é um objeto
+
 fun main() {
-
-    // no kotlin, todas as classes extends ANY == porque o kotlin atende qquer tipo de TIPO,
-    // a gente pode chamar todas as implementacoes da funcao println() abaixo.
-    println()
-    println(1)
-    println(1.0)
-    println(true)
-    println("teste")
-
     // sobrecarga implicita
     val endereco = Endereco(logradouro = "")
 
     // existe impressao objetos
-    println(endereco)
 
-    imprime()
-    imprimeInt(1)
-    imprimeTrue(1.0)
+    imprime(1)
+    imprime(1.0)
+    imprime(endereco)
 
 }
 
 // sobrecarga = diferentes implementacoes para uma mesma assinatura
-fun imprime() {}
-
-fun imprimeInt(int: Int) {}
-
-fun imprimeTrue(double: Double) {}
+fun imprime(valor: Any) {
+    println(valor)
+}
--- src/modelo/CalculadoraBonificacao.kt
+++ src/modelo/CalculadoraBonificacao.kt
@@ -7,7 +7,12 @@ class CalculadoraBonificacao {
     var total: Double = 0.0
     private set // garante q a property só sera modificada para quem esta interno
 
-    fun registra(funcionario: Funcionario) {
+    fun registra(funcionario: Any) {
+        // as == faz o casting
+        // funcionario as Funcionario
+
+        // is == faz verificacao + o casting
+        if (funcionario is Funcionario)
         this.total += funcionario.bonificacao
     }
 
