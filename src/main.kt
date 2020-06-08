8e41d6964b8a95a339f948122030377a6121bb9f
Fabiana Assami <bianavic@gmail.com>
2020-06-08 15:20:09 -0300

    functions types

--- src/main.kt
+++ src/main.kt
@@ -1,33 +1,15 @@
-import modelo.Endereco
+import teste.testaFuncaoAnonima
+import teste.testaFuncaoLambda
+import teste.testaTipoFuncaoClasse
+import teste.testaTipoFuncaoReferencia
 
-// PILHA DE EXECUCAO | CALL STACK
-
-// DEPURADOR | DEBUGGER
 
 fun main() {
-    println("Inicio main")
 
-    // simulando uma exception do tipo ArithmeticException ao tentar dividir por zero
-    10/0
-    funcao1()
-    println("Fim main")
-}
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
 
-fun funcao1() {
-    println("Inicio funcao1")
-    funcao2()
-    println("Fim funcao1")
-}
+    testaFuncaoAnonima()
+    testaFuncaoLambda()
 
-fun funcao2() {
-    println("Inicio funcao2")
-    for (i in 1..5) {
-        println(i)
-
-        // simulando uma exception do tipo ClassCastException
-        // ao tentar converter uma classe de um tipo nao compativel
-//        val endereco = Any()
-//        endereco as Endereco
-    }
-    println("Fim funcao2")
 }
