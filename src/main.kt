78563b2f8ba4bd79120e095f88bd7ff038424e6f
Fabiana Assami <bianavic@gmail.com>
2020-06-09 14:15:36 -0300

    outras funcoes de escopo

--- src/main.kt
+++ src/main.kt
@@ -14,7 +14,7 @@ fun main() {
         println("realizar pagamento")
     })
 
-    println("-----------------------------------------------------------")
+    println("----------------------IT-------------------------------------")
 
     // SCOPE FUNCTIONS == precisamos nos atentar ao RETORNO que desejamos com essas funcoes
     // ex: toUpperCase()
@@ -24,7 +24,7 @@ fun main() {
         .let { endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
         }.let(::println)
 
-    println("-----------------------------------------------------------")
+    println("-------------------------THIS----------------------------------")
 
     // Receiver (THIS) com retorno do objeto de contexto.
     // ao inves de devolver o objeto de contexto ele devolveu o THIS,
@@ -44,5 +44,54 @@ fun main() {
             println(enderecoMaiusculo)
         }
 
+    // aplique essas atribuicoes/configuracoes para este objeto"
+    println("-------------------------APPLY----------------------------------")
+
+    val enderecoApply = Endereco()
+        .apply {
+        logradouro = "rua vergueiro"
+        numero = 3185
+    }
+
+    println(enderecoApply)
+
+    // "COM este objeto, faca X)
+    println("-------------------------WITH----------------------------------")
+
+    // obs: WITH nao é uma extensao, por isso esta sem o ponto [.],
+    // por isso enviamos o objeto como um argumento
+
+    val enderecoWith = Endereco(logradouro = "rua vergueiro", numero = 3185)
+
+    with(enderecoWith) {
+        "$logradouro, $numero".toUpperCase()
+    }.let { enderecoMaiusculo: String ->
+        println(enderecoMaiusculo)
+    }
+
+
+    with(enderecoWith) {
+        "$logradouro, $numero".toUpperCase()
+    }.let { enderecoMaiusculo: String ->
+        println(enderecoMaiusculo)
+    }
+
+
+
+    println("-------------------------RUN----------------------------------")
+    // RUN se comporta como o LET ou como uma uma extensao
+    run { println("execucao do RUN sem extensao") }
+
+
+    // "e ALSO faz o seguinte com o objeto)
+    // exe de uso, casos de LOG
+    println("-------------------------ALSO----------------------------------")
+    // tendo ou nao ALSO no codigo ele nao interfere na logica geral do resultado
+    val enderecoAlso = Endereco()
+        .also { println("Criando endereco com ALSO") }
+        .apply {
+            logradouro = "rua vergueiro"
+            numero = 3185
+        }
 
 }
