7958e882981c0c21900b6364adbb405a322973ae
Fabiana Assami <bianavic@gmail.com>
2020-06-09 11:20:02 -0300

    autenticacao com  higher order functions

--- src/main.kt
+++ src/main.kt
@@ -1,47 +1,16 @@
-import modelo.Endereco
+import modelo.Autenticavel
+import modelo.SistemaInterno
 
 fun main() {
 
-    // ao trabalhar com objeto precisa criar uma variavel, a  nao ser que vc crie um metodo
-//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
-//    val enderecoMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-//    println(enderecoMaiusculo)
-
-
-    // HOF: nao é necessario criar a variavel
-    // HOF: sem retorno
-    Endereco(logradouro = "rua vergueiro", numero = 3185)
-        .let {
-        endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    // criar um autenticavel
+    val autenticavel = object : Autenticavel {
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
     }
 
-    // HOF: com retorno EXPLICITO
-    // o tipo funcao sempre retorna algo, Generics, mas podemos alterar conforme a funcao
-    val enderecoEmMaiusculo = Endereco(logradouro = "rua vergueiro", numero = 3185)
-        .let {
-                endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-        }
-    println(enderecoEmMaiusculo)
-
-    // HOF: podemos utilizar o RETORNO de outra forma, mudando para outro LET
-    // sao ESCOPOS DE FUNCOES onde cada uma faz sua responsabilidade
-    Endereco(logradouro = "rua vergueiro", numero = 3185)
-        .let {
-                endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-        }.let {
-                enderecoEmMaiusculo -> println(enderecoEmMaiusculo)
-        }
-
-    // HOF com RETORNO mais enxuto
-    Endereco(logradouro = "rua vergueiro", numero = 3185)
-        .let {
-                endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-        }.let (::println)
-
-    // LISTA
-    listOf(Endereco(complemento = "casa"),
-        Endereco(),
-        Endereco(complemento = "apartamento"))
-        .filter { endereco -> endereco.complemento.isNotEmpty()}
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("realizar pagamento")
+    })
 
 }
